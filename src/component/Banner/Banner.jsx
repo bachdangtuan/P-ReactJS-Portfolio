bcb83f4ca3db95120b23db3a6f81fb941fcaa720
bachdangtuan <tuan@gmail.com>
2022-03-05 22:33:48 +0700

    update fade in

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../../CSS/banner.css';
 import DownloadCV from './DownloadCV';
+import FadeIn from 'react-fade-in';
 
 
 class Banner extends Component {
@@ -19,13 +20,17 @@ class Banner extends Component {
                         </div>
                         <div className='row align-items-center'>
                             <div className='col-md-6 banner-content'>
-                                <h2 className='text-primary' >My Name</h2>
-                                <h1>BACH DANG
-                                    <span className='text-primary'> TUAN</span>
-                                </h1>
-                                <p>Life is a series of challenges.
-                                    <br /> I don't believe I can't<br /> As for the way, I still walk</p>
+                                <FadeIn transitionDuration={1000}>
+                                    <h2 className='text-primary' >My Name</h2>
+                                    <h1>BACH DANG
+                                        <span className='text-primary'> TUAN</span>
+                                    </h1>
+                                </FadeIn>
+                                <FadeIn transitionDuration={1000} delay={1000}>
+                                    <p>Life is a series of challenges.
+                                        <br /> I don't believe I can't<br /> As for the way, I still walk</p>
                                 <DownloadCV></DownloadCV>
+                                </FadeIn>   
                             </div>
                             <div className='col-md-6'>
                                 <div className='move-box'>
