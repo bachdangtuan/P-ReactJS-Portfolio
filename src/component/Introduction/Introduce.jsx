028106268951accf286b4b157720972c2b33e343
bachdangtuan <tuan@gmail.com>
2022-03-09 01:32:13 +0700

    update

--- src/component/Introduction/Introduce.jsx
+++ src/component/Introduction/Introduce.jsx
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import Frontend from './Frontend';
+import axios from 'axios';
 
 class Introduce extends Component {
+    state = {
+        dataProduct: []
+      }
+      componentDidMount() {
+        axios.get(`https://622467d23af069a0f9b511e7.mockapi.io/TuanAPI`)
+          .then(res => {
+            const dataProduct = res.data;
+            console.log(dataProduct);
+            this.setState({ dataProduct });
+          })
+          .catch(error => console.log(error));
+      }
+      
     render() {
         return (
             <div className="container">
