fa1499164514aeee7069971e636b2c339f1c5fe9
bachdangtuan <tuan@gmail.com>
2022-03-13 20:02:11 +0700

    update

--- src/Screen/Loadscreen.jsx
+++ src/Screen/Loadscreen.jsx
@@ -3,33 +3,20 @@ import React, { Component } from 'react';
 class Loadscreen extends Component {
     test = window.setInterval(function() {
         window.location.href = '/home';
-    }, 1000);
+    }, 1500);
     
     render() {
         return (
             <div className='bg-dark position-relative' style={{zIndex:9000}}>
-
-                    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis facere corrupti excepturi consectetur dolor tenetur nostrum numquam, iusto doloremque quaerat veritatis eum ad minima esse quod ratione? Omnis, libero consequuntur?
-                    Ipsam iusto culpa velit inventore, assumenda asperiores incidunt rerum, nobis facilis ad quidem, provident deleniti voluptates nesciunt temporibus in maiores! Dolore cum repudiandae quasi numquam reiciendis porro quam architecto amet.
-                    Error aspernatur dolores placeat officia modi magnam quibusdam tempora quasi quaerat. Pariatur sint incidunt dicta nobis tempore ipsum officia! Quod aspernatur amet ducimus reiciendis ex eos non culpa maiores laboriosam.
-                    Harum quam veritatis repellendus deserunt fugit dolores doloribus minima possimus ea quae nihil, omnis dolor incidunt. Iusto, aliquid corrupti soluta deleniti dignissimos sunt adipisci qui velit asperiores! Omnis, harum alias!
-                    Ab deserunt eius iure dolorum quia eveniet, natus corporis! Rerum, sed saepe facere praesentium assumenda modi minima. Dolore ea adipisci, repudiandae tenetur saepe, doloribus accusamus in maiores, incidunt tempora nam?
-                    Sunt cumque reprehenderit perspiciatis delectus. Perferendis, qui, temporibus soluta incidunt hic magnam, ullam ex nulla earum doloribus explicabo. Laborum modi alias aut assumenda consequuntur enim facilis soluta eius facere iure?
-                    Adipisci at assumenda cumque laudantium illo! Dolore nam ullam, illum deleniti et magni tempora, voluptate quo, impedit provident facilis? Totam aspernatur commodi unde praesentium sint animi at dolor. Quibusdam, ab!
-                    Asperiores et in a sequi esse quas animi eius nostrum alias reprehenderit molestiae voluptatem maxime ipsum recusandae sit id, numquam corrupti repudiandae nemo beatae odio vel dolore aut? Quisquam, perferendis.
-                    Ex qui tenetur corporis cum aliquam maiores et odit error dicta ipsa earum quaerat reiciendis vitae accusamus deserunt voluptatum, porro provident. Sequi nam facere quaerat. Numquam autem omnis ipsa iusto.
-                    Molestias repellat quasi perspiciatis a suscipit nemo. Laborum laboriosam expedita deserunt. Et, vero nam officia autem placeat sequi alias maxime obcaecati nisi itaque repellendus eum dicta rerum necessitatibus eligendi quos?
-                    Iure tempora consequatur nemo sapiente dolorum minima amet molestias a beatae, quas ut incidunt, quos, vel alias magnam? Ipsam facilis cum laudantium! Esse atque molestias, laboriosam repellendus quasi nostrum nihil.
-                    Quibusdam debitis maiores nisi neque quia, unde ut sequi at amet eaque soluta odio, harum ex quod. Deserunt nemo ex sunt, nisi, ullam nihil fugiat voluptates ad accusantium sint voluptatem.
-                    Similique aut beatae recusandae officiis repellendus, obcaecati porro voluptatem neque in fugit, doloribus nemo ex, inventore fuga alias voluptatibus cum? Ducimus numquam adipisci modi velit iure sunt accusantium recusandae beatae.
-                    Tempora enim dolorem rem. Cumque nobis nihil tempora quae sapiente odit dicta, natus praesentium earum placeat fugiat veritatis dolorum iure ab quaerat libero unde, iste reprehenderit qui molestiae reiciendis eligendi.
-                    Asperiores nihil accusamus illum sunt iure, quas voluptas obcaecati odit pariatur doloremque quibusdam numquam quis qui eaque corporis error magni provident perferendis debitis commodi laudantium incidunt. Molestias obcaecati iste officia.
-                    Qui consectetur ullam doloremque placeat, rem minus velit amet fuga reprehenderit ipsum, iure, ipsam nulla quasi! Illo, reprehenderit consectetur. Beatae minus officia ea aspernatur voluptatem illum laborum nostrum iure praesentium?
-                    Odit eveniet aut quis nulla at recusandae repellat quae atque, dolores assumenda accusantium labore. Distinctio obcaecati blanditiis molestiae dolor veniam corporis ipsa ullam molestias, et, debitis praesentium. Quis, odio id!
-                    Tenetur quidem veniam maxime laudantium quisquam quas sint deserunt nam quo iure ipsam totam unde, expedita, illo itaque harum assumenda, recusandae officiis ab accusantium officia quibusdam pariatur. Expedita, sed in!
-                    Natus itaque culpa non veritatis eos soluta dolores. Quo ex sint ipsa laborum alias consectetur ducimus odio quam animi sapiente. Earum ipsum omnis nulla neque impedit architecto aperiam repellat cumque.
-                    Corrupti consequuntur eligendi sed nesciunt, odio commodi laborum voluptate alias doloremque enim accusantium ipsam asperiores molestias expedita illo sapiente cumque tempore at error necessitatibus earum explicabo quasi recusandae voluptatibus! Quod.</h1>
-        
+                <div className="row m-auto w-100" 
+                style={{
+                    background:'#011831',
+                    height:'948px',
+                }}
+                
+                >
+                <img src={require('../img/loader1.gif')} className='m-auto'/>
+                </div>
            
             </div>
         );
