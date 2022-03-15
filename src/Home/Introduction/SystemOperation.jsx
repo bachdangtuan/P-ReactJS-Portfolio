aaad7e804b4047a9390d9b5ce9661b981fe3235c
Tuan Bach <bachdangtuan93@gmail.com>
2022-03-15 13:09:50 +0700

    update file

--- src/Home/Introduction/SystemOperation.jsx
+++ src/Home/Introduction/SystemOperation.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import ScrollAnimation from 'react-animate-on-scroll';
+import Fade from 'react-reveal/Fade';
+import Roll from 'react-reveal/Roll';
 
 export default class SystemOperation extends Component {
     renderSO = () => {
@@ -20,23 +22,19 @@ export default class SystemOperation extends Component {
             <div className="row align-items-center">
                 <div className="col-md-6">
                     <ScrollAnimation animateIn="slideInLeft">
-                    <h2>Systems Operation</h2>
+                        <h2>Systems Operation</h2>
                         {this.renderSO()}
                     </ScrollAnimation>
                 </div>
-                <div className="col-md-6 frontEnd">
-                    <ScrollAnimation animateIn="zoomIn">
-                        <img src={require('../../img/icon11.png')} />
-                        <img src={require('../../img/icon1.png')} />
-                        <img src={require('../../img/icon2.png')} />
-                        <img src={require('../../img/icon4.png')} />
-                        <img src={require('../../img/icon8.png')} />
-                        <img src={require('../../img/icon5.png')} />
-                        <img src={require('../../img/icon3.png')} />
-                        <img src={require('../../img/icon7.png')} />
-                        <img src={require('../../img/icon9.png')} />
-                        <img src={require('../../img/icon10.png')} />
-                    </ScrollAnimation>
+                <div className="col-md-6 frontEnd operation">
+                        <img src={require('../../img/so2.png')} />
+              
+                        <Fade>
+                            <img src={require('../../img/so1.png')} />
+                        </Fade>
+                        <img src={require('../../img/so4.png')} />
+                        <img src={require('../../img/so3.png')} />
+                        <img src={require('../../img/so5.png')} />
 
                 </div>
 
