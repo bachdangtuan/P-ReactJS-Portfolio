275b069eb6bc32b456bab229c11ca6494dac7f4e
bachdangtuan <tuan@gmail.com>
2022-03-21 01:16:11 +0700

    update

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 class Contact extends Component {
     render() {
@@ -11,10 +12,24 @@ class Contact extends Component {
                 </div>
 
                 <div className='container'>
+                    <div className='row align-items-center'>
+                        <div className='col-md-6 banner-content'>
+                            <ScrollAnimation animateIn="fadeInUp">
+                                <h1>CONTACT
+                                    <span className='text-primary'> ME</span>
+                                </h1>
+                            </ScrollAnimation>
+                        </div>
+                        <div className="col-md-6 frontEnd">
+                            <ScrollAnimation animateIn="fadeIn" delay={1000}>
+                                <img src={require('../img/m2.png')} />
+                                <img src={require('../img/m3.png')} />
+                                <img src={require('../img/m1.png')} />
+                                <img src={require('../img/m.png')} />
+                            </ScrollAnimation>
+                        </div>
 
-                    <p>Đây là trang Contact</p>
-
-
+                    </div>
                 </div>
             </div>
         );
