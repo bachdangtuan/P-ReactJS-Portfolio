77e32877403e5e25af2917a6d7f01fbaee6841cc
bachdangtuan <tuan@gmail.com>
2022-03-30 03:27:28 +0700

    update

--- src/Screen/Loadscreen.jsx
+++ src/Screen/Loadscreen.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 
 class Loadscreen extends Component {
-    test = window.setInterval(function() {
-        window.location.href = '/home';
-    }, 800);
-    
+    // test = window.setInterval(function() {
+    //     window.location.href = '/home';
+    // }, 800);
+
     render() {
         return (
-            <div className=' position-relative mh-100 w-100 loader' style={{zIndex:9430}}>
-                <div className="row m-auto w-100 loader__div " 
-                style={{
-                    background:'#011831',
-                    height:'100vh',  
-                }}
-                
+            <div className=' position-relative mh-100 w-100 loader' style={{ zIndex: 9430 }}>
+                <div className="row m-auto w-100 loader__div "
+                    style={{
+                        background: '#011831',
+                        height: '100vh',
+                    }}
+
                 >
-                <img src={require('../img/loader1.gif')} className='m-auto'/>
+                    <img src={require('../img/loader1.gif')} className='m-auto' />
                 </div>
-           
+
             </div>
         );
     }
