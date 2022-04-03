0e408ea4b1756a48feaa26f0392020fa69f363f7
BBach Dang Tuan <bachdangtuan93@gmail.com>
2022-04-04 01:50:59 +0700

    update Education v2

--- src/Education/Education.jsx
+++ src/Education/Education.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import ScrollAnimation from 'react-animate-on-scroll';
+import certification from '../data/certification.json';
 
 class Education extends Component {
+    renderCert = () => {
+        return certification.map((sp, index) => {
+            return <div className="col-md-4" key={index}>
+                <div class="card">
+                    <img className="card-img-top" src={sp.hinhAnh} alt="Card image cap" />
+                    <div class="card-body certification__">
+                        <h5>{sp.khoaHoc}</h5>
+                        <h6>-{sp.school}-</h6>
+                    </div>
+                </div>
+            </div>
+        })
+    }
     render() {
         return (
-
             <div className='container' style={{
                 paddingTop: '220px'
             }}>
@@ -27,7 +40,9 @@ class Education extends Component {
                                 </h1>
                                 <p>I am always looking and learning everywhere to improve my personal skills. The way that I teach myself is practice.
                                 </p>
-
+                                <img src="../img/logo/asroma.png" alt="" width={60} className="pt-2" />
+                                <img src="../img/logo/asroma.png" alt="" width={60} className="pt-2" />
+                                <img src="../img/logo/asroma.png" alt="" width={60} className="pt-2" />
                             </ScrollAnimation>
                             <br />
                         </div>
@@ -44,15 +59,62 @@ class Education extends Component {
                         </p>
                     </ScrollAnimation>
                     <br />
+                    <div className="row degrees pb-4">
+                        <div className="col-3 p-0">
+                            <ScrollAnimation animateIn="flipInY">
+                                <div className='degrees__'>
+                                    <img src="../img/logo/asroma.png" alt="" width={60} className="pt-2" />
+                                    <p>Fc Inter Milan</p>
+                                </div>
+                            </ScrollAnimation>
+                        </div>
+                        <div className="col-9 degrees__infor">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic fugiat porro in voluptatum, iste iusto illum! Fugiat atque provident dicta quos ad consequatur, ducimus voluptate odio id voluptatem dolor officia?
+                            Labore, maxime odio beatae debitis, consequatur ipsam quibusdam, sit vel commodi distinctio incidunt cum facere. Vitae eum nostrum nulla dicta libero. Minus distinctio sapiente suscipit quae ducimus ratione at repellat.
+                            Doloremque ut possimus, esse officiis iure explicabo praesentium neque blanditiis, autem molestias voluptatibus, cum consectetur vero vitae! Obcaecati unde totam reiciendis ipsum officiis accusamus nam. Dolores doloribus esse voluptatum provident!
+                        </div>
+                    </div>
+                    <div className="row degrees">
+                        <div className="col-3 p-0">
+                            <ScrollAnimation animateIn="flipInY">
+                                <div className='degrees__'>
+                                    <img src="../img/logo/asroma.png" alt="" width={60} className="pt-2" />
+                                    <p>Fc Inter Milan</p>
+                                </div>
+                            </ScrollAnimation>
+                        </div>
+                        <div className="col-9 degrees__infor">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic fugiat porro in voluptatum, iste iusto illum! Fugiat atque provident dicta quos ad consequatur, ducimus voluptate odio id voluptatem dolor officia?
+                            Labore, maxime odio beatae debitis, consequatur ipsam quibusdam, sit vel commodi distinctio incidunt cum facere. Vitae eum nostrum nulla dicta libero. Minus distinctio sapiente suscipit quae ducimus ratione at repellat.
+                            Doloremque ut possimus, esse officiis iure explicabo praesentium neque blanditiis, autem molestias voluptatibus, cum consectetur vero vitae! Obcaecati unde totam reiciendis ipsum officiis accusamus nam. Dolores doloribus esse voluptatum provident!
+                        </div>
+                    </div>
                 </div>
-                {/* Certification */}
+                {/* Certification for dev */}
                 <div className='container banner-content text-center pb-5'>
                     <ScrollAnimation animateIn="fadeInUp">
-                        <h1>MY
-                            <span className='text-primary'> CERTIFICATIONS </span>
+                        <h1>CERTIFICATIONS
+                            <span className='text-primary'> FOR DEV </span>
                         </h1>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni sequi nulla exercitationem quasi illo provident debitis voluptatibus nihil, ex id modi numquam voluptatem fuga doloribus aliquam, reiciendis perspiciatis architecto voluptate.
-                        </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elito!</p>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <div className='row'>
+                            {this.renderCert()}
+                        </div>
+                    </ScrollAnimation>
+                    <br />
+                </div>
+                {/* Certification other */}
+                <div className='container banner-content text-center pb-5'>
+                    <ScrollAnimation animateIn="fadeInUp">
+                        <h1>CERTIFICATIONS
+                            <span className='text-primary'> OTHER </span>
+                        </h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                        <p>Lorem ipsum dolor sit.</p>
+                        <div className='row'>
+                            {this.renderCert()}
+                        </div>
                     </ScrollAnimation>
                     <br />
                 </div>
